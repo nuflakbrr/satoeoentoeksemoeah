55f77fbca247904e3128a56f1cc7d46716d8d9dd
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-21 15:46:04 +0700

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from './pages'
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
