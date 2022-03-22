96c924fa712f4f6c05cc320a9ed87400476ee35a
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-22 11:40:21 +0700

    change tabindex to tabIndex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar() {
                                     <div>
                                         <div className='group relative'>
                                             <button className={`px-3 py-2 rounded-md text-sm font-medium ${inactiveClass}`}>Studi Kasus</button>
-                                            <nav tabindex='0' className='border-2 bg-white invisible border-gray-800 rounded w-60 absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1'>
+                                            <nav tabIndex='0' className='border-2 bg-white invisible border-gray-800 rounded w-60 absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1'>
                                                 <ul className='py-1'>
                                                     <li>
                                                         <NavLink to='#' className='block px-4 py-2 hover:bg-gray-100'>
