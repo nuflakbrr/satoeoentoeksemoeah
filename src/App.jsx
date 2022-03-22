34c0cfd094673d11a5b6a97f8a857401d3beff63
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-22 16:14:53 +0700

    create 404 page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import { About, BMI, Home } from './pages'
+import { About, BMI, Home, NotFound } from './pages'
 
 function App() {
   const [dark, setDark] = useState()
@@ -9,9 +9,10 @@ function App() {
   return (
     <div className={dark ? '' : 'dark'}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/bmi" element={<BMI />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/bmi' element={<BMI />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './Home'
 export { default as About } from './About'
 export { default as BMI } from './BMI'
+export { default as NotFound } from './NotFound'
