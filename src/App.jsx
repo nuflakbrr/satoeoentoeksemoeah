23dd41faff5427a962b49e4a2743e0f71d6755e1
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-23 13:35:30 +0700

    create router fot /cicilan

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import { About, BMI, Home, NotFound } from './pages'
+import { About, BMI, Cicilan, Home, NotFound } from './pages'
 
 function App() {
   const [dark, setDark] = useState()
@@ -12,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/bmi' element={<BMI />} />
+        <Route path='/cicilan' element={<Cicilan />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
